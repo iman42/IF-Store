be20dfe9e0fc7e3af115dc010c70ed65aadbcfe2
Yogesh Dhamija <ydhamija96@gmail.com>
2020-12-22 18:54:29 -0600

    added junit to maven

--- comic_store/src/main/java/com/ifstore/web/comic_store/ComicStoreApplication.java
+++ comic_store/src/main/java/com/ifstore/web/comic_store/ComicStoreApplication.java
@@ -2,21 +2,12 @@ package com.ifstore.web.comic_store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@RestController
 public class ComicStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComicStoreApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String helloWorld(@RequestParam(value = "name", defaultValue = "World") String name){
-		return ("hello " + name);
 	}
 }
