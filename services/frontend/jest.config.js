d868205ce3a489c419ed55c0de8a14c7439a8112
Yogesh Dhamija <ydhamija96@gmail.com>
2022-03-04 18:26:01 -0600

    fix frontend tests

--- services/frontend/jest.config.js
+++ services/frontend/jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-    setupFilesAfterEnv: ["<rootDir>/testUtils/setupTests.js"],
-    transform: {
-        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-        "\\.(css|less|scss|sass)$": "<rootDir>/testUtils/styleMock.js"
-    }
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  setupFilesAfterEnv: ["<rootDir>/testUtils/setupTests.js"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    "\\.(css|less|scss|sass)$": "<rootDir>/testUtils/styleMock.js",
+  },
+  testEnvironment: "jsdom",
 };
