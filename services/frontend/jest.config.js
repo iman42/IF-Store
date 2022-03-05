63b5b5b4e9be963b0fcf2b166ca302d8dd1ba1ab
Yogesh Dhamija <ydhamija96@gmail.com>
2022-03-04 18:27:00 -0600

    updated vscode formatter

--- services/frontend/jest.config.js
+++ services/frontend/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-  setupFilesAfterEnv: ["<rootDir>/testUtils/setupTests.js"],
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-    "\\.(css|less|scss|sass)$": "<rootDir>/testUtils/styleMock.js",
-  },
-  testEnvironment: "jsdom",
+    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+    setupFilesAfterEnv: ["<rootDir>/testUtils/setupTests.js"],
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+        "\\.(css|less|scss|sass)$": "<rootDir>/testUtils/styleMock.js",
+    },
+    testEnvironment: "jsdom",
 };
